628704080e70990b26da2e1184bb7c049a89b4a2
JakubRudniks <62304977+JakubRudniks@users.noreply.github.com>
2020-03-18 10:44:15 +0100

    Add files via upload

---
+++ zad 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+int t[119];
+t[0]=1;
+for(int i=0;i<7;i++)
+t[i]=0;	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
